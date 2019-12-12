c3c9aa5910d906bfb933b2920bd61cdea86f377d
Mike Gevaert <michael.gevaert@epfl.ch>
2019-12-12 13:55:30 +0100

    fix so cython plays well with sdist
    Change-Id: I626fac00a16c7af1fee73e53775ec01d8114bbdd

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 '''setup.py for archngv-building'''
 import imp
 from setuptools import setup, Extension, find_packages
-from Cython.Build import cythonize
+
+try:
+    from Cython.Build import cythonize
+    _USE_CYTHON = True
+except ImportError:
+    _USE_CYTHON = False
 
 VERSION = imp.load_source("archngv_building.version", "archngv_building/version.py").VERSION
 
-cython_extensions = [Extension("*", ["archngv_building/endfeet_reconstruction/*.pyx", ],),
-                     ]
+
+if _USE_CYTHON:
+    extensions = cythonize([Extension("*",
+                                      ["archngv_building/endfeet_reconstruction/*.pyx", ],),
+                            ])
+else:
+    from glob import glob
+    sources = glob("archngv_building/endfeet_reconstruction/*.c")
+    assert sources, 'Must have .c files in archngv_building/endfeet_reconstruction/'
+    extensions = [Extension(source.strip('.c'), [source])
+                  for source in sources]
+
+
 
 setup(
     classifiers=[
@@ -18,8 +34,11 @@ setup(
     author='Eleftherios Zisis',
     author_email='eleftherios.zisis@epfl.ch',
     packages=find_packages(),
-    ext_modules=cythonize(cython_extensions),
+    ext_modules=extensions,
     include_package_data=True,
+    setup_requires=[
+        'numpy>=1.13',
+    ],
     install_requires=[
         'numpy>=1.13',
     ],
